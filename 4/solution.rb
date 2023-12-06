e646ab8b3bf6d8ccf2bf2525a47415bcf4c78a15
Charlie <charlie.marlow@alto.com>
2023-12-05 20:40:42 -0800

    4 part 2

--- 4/solution.rb
+++ 4/solution.rb
@@ -34,3 +34,39 @@ sum = lines.sum do |line|
 end
 
 puts sum
+
+# Part 2
+def score(winning, having)
+  winning = Set.new(winning)
+
+  result = 0
+  having.each do |num|
+    result += 1 if winning.include?(num)
+  end
+
+  result
+end
+
+cards_by_num = Hash.new(0)
+max_card_num = lines.length
+
+sum = lines.sum do |line|
+  card, numbers = line.split(':')
+
+  card_num = card.split(' ')[1].to_i
+  cards_by_num[card_num] += 1
+
+  winning, having = numbers.split('|').map do |nums|
+    nums.split(' ').map(&:to_i)
+  end
+  points = score(winning, having)
+
+  upper_limit = [max_card_num, card_num + points].min
+  (card_num + 1..upper_limit).each do |i|
+    cards_by_num[i] += cards_by_num[card_num]
+  end
+
+  cards_by_num[card_num]
+end
+
+puts sum
