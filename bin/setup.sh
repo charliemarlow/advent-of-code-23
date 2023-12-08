aeb4d4d5f7dd819dcddb616036a8b7c6eecf28b0
Charlie <charlie.marlow@alto.com>
2023-12-08 08:22:01 -0800

    tweak

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,3 +3,6 @@ touch "$1/solution_part1.rb"
 touch "$1/solution_part2.rb"
 touch "$1/input.txt"
 touch "$1/test.txt"
+
+echo "file = File.open('$1/test.txt')\n" > "$1/solution_part1.rb"
+echo "file = File.open('$1/test.txt')\n" > "$1/solution_part2.rb"
